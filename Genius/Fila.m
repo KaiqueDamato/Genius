4b841007941e6385e4d6d8c58cb57669053adcb8
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-02-13 14:22:47 -0200

    Métodos Fila

--- Genius/Fila.m
+++ Genius/Fila.m
@@ -8,11 +8,25 @@
 
 #import "Fila.h"
 
+
 @implementation Fila
 
-- (NSInteger)randomNumberBetween:(NSInteger)min maxNumber:(NSInteger)max
-{
+- (NSInteger)randomNumberBetween:(NSInteger)min maxNumber:(NSInteger)max {
+    
     return min + arc4random_uniform(max - min + 1.0);
 }
+
+-(void)inserirArrayPergunta:(NSInteger)random {
+    
+    [_arrayPergunta addObject:random];
+}
+-(void)inserirArrayResposta:(NSInteger)resposta {
+    [_arrayResposta addObject:random];
+}
+
+-(void)compararArrays {
+    
+}
+-(void)limparArrays;
 
 @end
--- Genius/Genius/main.m
+++ Genius/Genius/main.m
@@ -8,12 +8,15 @@
 
 #import <Foundation/Foundation.h>
 #import "Fila.h"
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        Fila *t = [[Fila alloc]init];
+    Fila *t = [[Fila alloc]init];
     NSLog(@"%li", [t randomNumberBetween:1 maxNumber:2]);
+        
+    system( "clear" );
         
     }
     return 0;
